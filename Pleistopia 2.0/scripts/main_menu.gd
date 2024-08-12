423a56988dfd56251c8c1d38a9519bb43ac5725a
Ghislain Thiery <ghislain.thiery@ntymail.com>
2024-08-12 19:14:28 +0200

    Quit button

--- Pleistopia 2.0/scripts/main_menu.gd
+++ Pleistopia 2.0/scripts/main_menu.gd
@@ -7,3 +7,12 @@ func _ready():
 
 func _on_button_new_game_pressed():
 	get_tree().change_scene_to_file("res://scenes/world.tscn")
+
+func _on_button_load_game_pressed():
+	pass # Replace with function body.
+
+func _on_button_settings_pressed():
+	pass # Replace with function body.
+
+func _on_button_quit_pressed():
+	get_tree().quit()
